5c7625c708a34292a446bbc3800fb3b927e4a845
Mohamed Abohend <97126553+Abohend@users.noreply.github.com>
2022-12-29 09:36:14 +0200

    Add files via upload
    Added trigger to make account for the new clients

--- Bank.sql
+++ Bank.sql
@@ -1,7 +1,7 @@
-create database Bank
+create database MyBank
 go
 
-use Bank;
+use MyBank;
 
 CREATE TABLE employee (
 	ssn	CHAR(9) primary key,
@@ -10,6 +10,9 @@ CREATE TABLE employee (
 	start_date date NOT NULL,
 	manager_ssn CHAR(9)
 	)
+	
+/*  modifing unique value constraint */
+alter table employee add unique (phone_num)
 
 /* add manager id as foreign key */
 ALTER TABLE employee
@@ -31,10 +34,11 @@ create table dependent(
 	)
 
 create table account(
-	number int primary key,
-	balance float ,
+	number int primary key identity(1,1),
+	balance float default 0,
 	owner_ssn char(9) foreign key references customar(ssn)
 	)
+
 
 create table saving_account(
 	number int foreign key references account(number),
@@ -53,7 +57,7 @@ create table branch(
 	)
 
 create table loan(
-	number int primary key,
+	number int primary key identity(1,1),
 	amount float not null,
 	customar_ssn char(9) foreign key references customar(ssn),
 	branch_name varchar(10) foreign key references branch(name),
@@ -61,7 +65,7 @@ create table loan(
 	)
 
 create table payment(
-	number int primary key, 
+	number int primary key identity(1,1), 
 	loan_no int not null foreign key references loan(number),
 	amount float not null,
 	date date
@@ -74,3 +78,56 @@ create table trans(
 	type char not null,
 	amount float not null
 	)
+
+
+
+/* Insert employees data */
+insert into employee 
+values (780924598, 'Saad Eldaly', 01111111111, '2001-02-13', null),
+	(193702892, 'Johnny depp', 01000000000, '2002-02-12', 780924598),
+	(11111111, 'Adel Shakal', 01113111111, '2010-12-13', 193702892),
+	(222222222, 'Johnny depp', 01040500000, '2002-02-12', 780924598),
+	(333333333, 'Soaad Hosny', 01040000000, '2015-03-01', 780924598)
+
+/* create trigger to make account for the new customars */
+create trigger new_account
+on customar after insert
+as
+	begin
+		declare @ssn char(9);
+		select @ssn = e.ssn from inserted e;
+		insert into account(owner_ssn) values (@ssn)
+	end
+
+drop trigger new_account
+delete customar
+delete account
+select * from account
+
+/* insert customars with new accounts */
+insert into customar
+values (444444444, 'Mohamed Abohend', 'Gharbia', 780924598);
+insert into customar
+values(555555555, 'Mohamed Elshorbagy', 'Menofia', 193702892);
+insert into customar
+values(666666666, 'Ebrahim', 'Alexandria', 193702892);
+insert into customar
+values(999999999, 'Mohamed Elsha7at', 'Marsa Matro7', 222222222);
+insert into customar
+values(123456789, 'Mohamed Konsowa', 'El3lmeen', 333333333);
+
+
+/* create accounts as saving accounts or checking accounts */
+insert into saving_account
+values
+
+
+create trigger deposite
+	on account after insert
+	as
+	begin
+		/* if the account is saving account */
+		/* insert into the interest account upon on saving account */
+		/* else (the account is checking account */
+		/* deal with it */
+	end
